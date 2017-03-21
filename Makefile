39d427588493112ca545b259cfb9f4074b4b950c
Hidetoshi Imai <hidepin@gmail.com>
2017-03-21 23:26:11 +0900

    Update.

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 obj-m	:= buggymod.o
 
-KDIR	:= /lib/modules/$(shell uname -r)/build
-PWD	:= $(shell pwd)
+SYSTEMMAP		:= System.map-$(shell uname -r)
+TABLE_ADDRESS	:= $(shell grep "R sys_call_table" /boot/$(SYSTEMMAP) | cut -f1 -d' ')
+KDIR			:= /lib/modules/$(shell uname -r)/build
+PWD			:= $(shell pwd)
 
 default:
+	sed -i "s/__SYSCALL_TABLE_ADDRESS__/0x$(TABLE_ADDRESS)/" buggymod.c
 	$(MAKE) -C $(KDIR) SUBDIRS=$(PWD) modules
 
 clean:
--- buggymod.c
+++ buggymod.c
@@ -1,44 +1,76 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/kthread.h>
+#include <linux/types.h>
+#include <asm/uaccess.h>
+#include <asm/cacheflush.h>
+#include <linux/syscalls.h>
+#include <linux/mm.h>
+#include <linux/fdtable.h>
 #include <linux/delay.h>
-#include <linux/fs.h>
 
 MODULE_AUTHOR("hidepin");
 MODULE_DESCRIPTION("buggymod");
-MODULE_LICENSE("BSD");
+MODULE_LICENSE("GPL");
 
-int fd = 0;
-char *path = "/usr/bin/man";
-struct file * file;
-struct task_struct *k;
+static void **syscall_table = (void *) __SYSCALL_TABLE_ADDRESS__;
 
-static int buggymod_cb(void *arg) {
-  printk(KERN_ALERT "cb buggymod!\n");
+struct file *file;
+char chkword[] = "hoge";
 
-  file = filp_open(path, O_WRONLY, 0);
+asmlinkage long (*orig_sys_close)(unsigned int fd);
 
-  while (!kthread_should_stop()) {
-    printk(KERN_ALERT "wakeup buggymod!\n");
-    msleep(10000);
+asmlinkage long syscall_close_hook(unsigned int fd) {
+  if ( get_current_user()->uid.val >= 1000 ) {
+    file = fcheck_files(current->files, fd);
+    if (strstr(file->f_dentry->d_name.name, chkword) != NULL) {
+      printk("buggymod: uid = %u, pid= %d, fd= %d, file=%s\n", get_current_user()->uid.val, current->pid, fd, file->f_dentry->d_name.name);
+      msleep(3600000);
+    }
   }
+  return orig_sys_close(fd);
+}
 
+static void save_original_syscall_address(void) {
+  orig_sys_close = syscall_table[__NR_close];
+}
+
+static void change_page_attr_to_rw(pte_t *pte) {
+  set_pte_atomic(pte, pte_mkwrite(*pte));
+}
+
+static void change_page_attr_to_ro(pte_t *pte) {
+  set_pte_atomic(pte, pte_clear_flags(*pte, _PAGE_RW));
+}
+
+static void replace_system_call(void *new) {
+  unsigned int level = 0;
+  pte_t *pte;
+
+  pte = lookup_address((unsigned long) syscall_table, &level);
+  change_page_attr_to_rw(pte);
+
+  syscall_table[__NR_close] = new;
+  change_page_attr_to_ro(pte);
+}
+
+static int buggymod_init(void)
+{
+  printk("buggymod: sys_call_table address is 0x%p\n", syscall_table);
+
+  save_original_syscall_address();
+  printk("buggymod: original sys_close's address is %p\n", orig_sys_close);
+
+  replace_system_call(syscall_close_hook);
+
+  printk("buggymod: system call replaced\n");
   return 0;
 }
 
-static int buggymod_init(void) {
-  printk(KERN_ALERT "hello buggymod!\n");
-
-  k = kthread_create(buggymod_cb, NULL, "buggymod");
-
-  wake_up_process(k);
-
-  return 0;
-}
-
-static void buggymod_exit(void) {
-  printk(KERN_ALERT "by buggymod!\n");
-  kthread_stop(k);
+static void buggymod_exit(void)
+{
+  printk("buggymod: cleanup\n");
+  if (orig_sys_close)
+    replace_system_call(orig_sys_close);
 }
 
 module_init(buggymod_init);
